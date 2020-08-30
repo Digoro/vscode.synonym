da17ed60b676a2c7671075186647b9e43a3efd5a
digoro <lsh000124@gmail.com>
2020-08-31 00:10:41 +0900

    register command "find sysnonym"

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import fetch from 'node-fetch';
 import * as vscode from 'vscode';
+import { window } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const API_URL = 'https://api.datamuse.com/words?ml=';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "synonym-finder" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('synonym-finder.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from synonym-finder!');
+	vscode.commands.registerCommand("synonym.find", async () => {
+		const editor = window.activeTextEditor;
+		const text = editor?.document.getText(editor.selection);
+		fetch(`${API_URL}${text}`).then(async (resp) => {
+			const words = await resp.json();
+			if (!words) {
+				window.showWarningMessage("No synonyms found. Please check if the word entered is correct.");
+				return;
+			} else {
+				if (words.length === 0) {
+					window.showWarningMessage("No synonyms found. Please check if the word entered is correct.");
+					return;
+				}
+			}
+			const quickPick = window.createQuickPick();
+			quickPick.items = words.map((word: any) => ({ label: word.word }));
+			quickPick.onDidChangeSelection(([item]) => {
+				if (item) {
+					editor?.edit(edit => edit.replace(editor.selection, item.label));
+					quickPick.dispose();
+				}
+			});
+			quickPick.onDidHide(() => quickPick.dispose());
+			quickPick.show();
+		}).catch(error => {
+			window.showWarningMessage("No synonyms found. Please check if the word entered is correct.");
+		});
 	});
-
-	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
